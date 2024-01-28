acb9bd5659f0c8c58d7a8204308ae1393547dc67
Ameeq-coder <ahmadameeq19@gmail.com>
2024-01-28 20:02:25 +0500

    Changes To Gradle

--- app/src/main/java/com/example/studysquad/loginsignup/SignupScreen.kt
+++ app/src/main/java/com/example/studysquad/loginsignup/SignupScreen.kt
@@ -1,24 +1,31 @@
 package com.example.studysquad.loginsignup
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.studysquad.R
 import com.example.studysquad.ui.theme.StudySquadTheme
-import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun Signup(authViewModel: AuthViewModel){
-    Column(modifier = Modifier.fillMaxSize()) {
-      Box(modifier = Modifier, contentAlignment = Alignment.Center){
+fun Signup(){
+    Column(modifier = Modifier
+        .background(Color.White)
+        .fillMaxSize(),) {
+      Box(modifier = Modifier.padding(25.dp), contentAlignment = Alignment.Center){
           Image(painter = painterResource(id = R.drawable.loginsignup), contentDescription = null )
       }
+      Text(text = "Study Squads")
     }
 
 }
@@ -26,7 +33,7 @@ fun Signup(authViewModel: AuthViewModel){
 @Composable
 fun SignupPreview(){
     StudySquadTheme {
-        Signup(authViewModel = AuthViewModel(firebaseAuth = FirebaseAuth.getInstance())) // Pass the required parameters to Signup function
+        Signup() // Pass the required parameters to Signup function
     }
 
 }
