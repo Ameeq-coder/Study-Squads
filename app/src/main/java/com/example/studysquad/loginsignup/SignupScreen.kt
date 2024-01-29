3869b092e4608a999d57be0b4aeb9bd9c6ef9253
Ameeq-coder <ahmadameeq19@gmail.com>
2024-01-29 17:15:44 +0500

    New Changes

--- app/src/main/java/com/example/studysquad/loginsignup/SignupScreen.kt
+++ app/src/main/java/com/example/studysquad/loginsignup/SignupScreen.kt
@@ -6,32 +6,55 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.studysquad.R
 import com.example.studysquad.ui.theme.StudySquadTheme
 
 @Composable
-fun Signup(){
-    Column(modifier = Modifier
-        .background(Color.White)
-        .fillMaxSize(),) {
-      Box(modifier = Modifier.padding(25.dp), contentAlignment = Alignment.Center){
-          Image(painter = painterResource(id = R.drawable.loginsignup), contentDescription = null )
-      }
-      Text(text = "Study Squads")
+fun Signup() {
+    val emailstate = remember { mutableStateOf("") }
+    val passwordstate = remember { mutableStateOf("") }
+    val confirmstate = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+    ) {
+        Box(modifier = Modifier, contentAlignment = Alignment.TopCenter) {
+            Image(painter = painterResource(id = R.drawable.loginsignup), contentDescription = null)
+        }
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = "Study Squads",
+            color = colorResource(id = R.color.lightblue),
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold),
+            fontFamily = FontFamily.Cursive
+        )
+        OutlinedTextField(value = emailstate.value,
+            onValueChange = { emailstate.value = it },
+            label = { Text(text = "Email")})
     }
 
 }
+
 @Preview(showBackground = true)
 @Composable
-fun SignupPreview(){
+fun SignupPreview() {
     StudySquadTheme {
         Signup() // Pass the required parameters to Signup function
     }
